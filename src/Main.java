2f81d54e88e997242e8bbff9224af7349a6ae42d
Jesse <Jesse.Garcia@griffithuni.edu.au>
2018-09-30 12:14:28 +1000

    All games completed, just need to fix scoring

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ class Player {
         } else {
             credits -= wlCredits;
         }
-        System.out.println("Your current credit account is: " + getCredits());
+        System.out.println("Your current credit account is: " + getCredits() + "\n");
     }
 
     public void takeWager() {
@@ -55,7 +55,7 @@ class Player {
 
 }
 
-
+/*This Is the Pick a Number game.*/
 class PickANumber extends Player {
     private int winNumber;
 
@@ -94,54 +94,56 @@ class PickANumber extends Player {
     }
 }
 
-/*This is the No Match Dealer game.
- * It returns -1 if the player has 0 credits*/
-/*class NoMatchDealer extends Player {
+/*This is the No Match Dealer game.*/
+class NoMatchDealer extends Player {
+    private int i, j, wager =-1,match =-1;
+    private int[] numbers = new int[16];
 
-int dealer_no_match(){
-    int i, j, numbers[16], wager = -1, match = -1;
+    public void runGame() {
 
-    System.out.println("---==[ No Match Dealer ]==---");
-    System.out.println("In this game, you can wager up to all of your credits.\n");
-    System.out.println("The dealer will deal out 16 random numbers between 0 and 99.\n");
-    System.out.println("If there are no matches among them, you double your money!\n\n");
+        System.out.println("---==[ No Match Dealer ]==---");
+        System.out.println("In this game, you can wager up to all of your credits.");
+        System.out.println("The dealer will deal out 16 random numbers between 1 and 99.");
+        System.out.println("If there are no matches among them, you double your money!");
 
-    if(player.credits == 0){
-        System.out.println("You don't have any credits to wager!\n\n");
-        return -1;
-    }
-    while(wager == -1)
-        wager = take_wager(player.credits, 0);
+        if (getCredits() == 0) {
+            System.out.println("You don't have any credits to wager!");
+            return;
+        }
+        takeWager();
+        wager = getWager();
 
-    System.out.println("---==[ Dealing out 16 random numbers ]==---");
-    for (i = 0; i < 16; i++) {
-        numbers[i] = rand()%100;
-        System.out.println("%2d\t", numbers[i]);
-        if(i%8 == 7)
-            System.out.println("\n");
-    }
-    for (i = 0; i < 15; i++) {
-        j = i + 1;
-        while(j < 16){
-            if(numbers[i] == numbers[j])
-                match = numbers[i];
-            j++;
+        Random rand = new Random();
+
+        System.out.println("---==[ Dealing out 16 random numbers ]==---");
+        for (i = 0; i < 16; i++) {
+            int ace =
+            numbers[i] = rand.nextInt(100);
+            System.out.print(numbers[i] + "\t");
+            if (i % 8 == 7)
+                System.out.println(" ");
+        }
+        System.out.println(" ");
+        for (i = 0; i < 15; i++) {
+            j = i + 1;
+            while (j < 16) {
+                if (numbers[i] == numbers[j])
+                    match = numbers[i];
+                j++;
+            }
+        }
+        if (match != -1) {
+            System.out.println("The dealer matched the number " + match + "!");
+            System.out.println("You lose " + wager + " credits.");
+            updateCredits(wager, "lose");
+        } else {
+            System.out.println("There were no matches! You win " + wager + " credits!\n");
+            updateCredits(wager, "win");
         }
     }
-    if(match != -1){
-        System.out.println("The dealer matched the number %d!\n", match);
-        System.out.println("You lose %d credits.\n", wager);
-        player.credits -= wager;
-    }
-    else{
-        System.out.println("There were no matches! You win %d credits!\n", wager);
-        player.credits += wager;
-    }
-    return 0;
-}*/
+}
 
-/*This is the Find the Ace game.
- * It returns -1 if the player has 0 credits*/
+/*This is the Find the Ace game.*/
 class AceGame extends Player {
     private int i;
     private int pick = -1;
@@ -178,6 +180,7 @@ class AceGame extends Player {
             System.out.println("Select a card: 1, 2, or 3:  ");
             pick = input.nextInt();
         }
+        pick--;
         i = 0;
         while (i == ace || i == pick)
             i++;
@@ -191,8 +194,25 @@ class AceGame extends Player {
             char choiceTwo = '\n';
             while (choiceTwo == '\n')
                 choiceTwo = input.next().charAt(0);
+            if(choiceTwo == 'c' || choiceTwo == 'C'){
+                i = 0;
+                while(i == pick || cards[i] == 'Q')
+                    i++;
+                pick = i;
+                System.out.println("Your card pick has been changed to card ");
+                printCards(cards, pick);
+            }
             if (choiceTwo == 'k' || choiceTwo == 'K') {
                 invalidChoice = false;
+                i = 0;
+                while (i < 3) {
+                    if (i == ace) {
+                        cards[i] = 'A';
+                    } else {
+                        cards[i] = 'Q';
+                    }
+                    i++;
+                }
                 printCards(cards, pick);
                 if (pick == ace) {
                     System.out.println("Congratulations! You are a Winner!");
@@ -206,7 +226,7 @@ class AceGame extends Player {
             if (choiceTwo == 'i' || choiceTwo == 'I') {
                 System.out.println("How much would you like to wager of your current: " + getCredits() + " Credits?");
                 int wagerTwo = input.nextInt();
-                while(wagerTwo > getCredits()){
+                while (wagerTwo > getCredits()) {
                     System.out.println("Sorry you don'd have enough for that wager. Try again.");
                     wagerTwo = input.nextInt();
                 }
@@ -225,12 +245,15 @@ class AceGame extends Player {
         if (pick == -1) {
             System.out.println("\t\t 1 \t 2 \t 3\n");
         } else {
-            for (i = 0; i < pick; i++)
-                System.out.println("\t");
-            System.out.println(" ^-- your pick\n");
+            pick++;
+            for (i = 0; i <= pick; i++)
+                System.out.print("\t");
+            System.out.print(" ^-- your pick\n");
         }
     }
-};
+}
+
+;
 
 
 public class Main {
@@ -249,11 +272,11 @@ public class Main {
             p1.setName(newName);
         }
         while (menu) {
-            System.out.println("--==[ Game of Chance Menu ]==--");
+            System.out.println("---===[ Game of Chance Menu ]===---");
             System.out.println("1 - Play Pick a Number Game");
             System.out.println("2 - Play the No Match Dealer Game");
             System.out.println("3 - Play Find the Ace Game");
-            System.out.println("4 - View current high score");
+            System.out.println("4 - View current credits");
             System.out.println("5 - Change player name");
             System.out.println("6 - Reset Game");
             System.out.println("7 - Quit");
@@ -269,13 +292,16 @@ public class Main {
                     newGame.runGame();
                 } else if (i == 2) {
                     //noMatchGame;
+                    NoMatchDealer newGame = new NoMatchDealer();
+                    newGame.runGame();
                 } else if (i == 3) {
                     //findAce;
                     AceGame newGame = new AceGame();
                     newGame.runGame();
                 }
             } else if (i == 4) {
-                //viewScore;
+                //viewcredits;
+                System.out.println(p1.getCredits());
             } else if (i == 5) {
                 //changePlayer
                 System.out.println("Changing player name will reset credits as well.");
